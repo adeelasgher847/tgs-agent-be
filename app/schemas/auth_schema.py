7a5cc7ca207e76b2307a1d4cdb7daa81888e5913
Ali Saeed <alisaeed@thetgs.com>
2025-07-28 13:12:53 -0700

    auth

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, EmailStr
 
+
 class RegisterSchema(BaseModel):
     email: EmailStr
     password: str
     tenant_id: int
+
 
 class Token(BaseModel):
     access_token: str
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,24 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
+
+
 class UserBase(BaseModel):
     first_name: str
     last_name: str
-    email: str
+    email: EmailStr
     phone: Optional[str] = None
+
+
 class UserCreate(UserBase):
     password: str
+
+
 class UserOut(UserBase):
     id: int
     join_date: datetime
     created_at: datetime
+
     class Config:
         orm_mode = True
-        
+
+
 class UserLogin(BaseModel):
     email: str
     password: str
 
+
 class Token(BaseModel):
     access_token: str
-    token_type: str        
+    token_type: str
