3453fcaeb96454c4c77737bfe8abe9f5108f0255
Mubeen Hussain <mubeen.hussain@thetgs.com>
2025-08-05 22:43:33 +0500

    call endpoints in api router

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import user, tenant, role, voice_agent
+from app.api.api_v1.endpoints import user, tenant, role
+from app.routers.agent import router as agent_router
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(tenant.router, prefix="/tenants", tags=["tenants"])
 api_router.include_router(role.router, prefix="/roles", tags=["roles"]) 
-api_router.include_router(voice_agent.router, prefix="/agent", tags=["Voice Agent"]) 
+api_router.include_router(agent_router, prefix="/agent", tags=["Voice Agent"]) 
