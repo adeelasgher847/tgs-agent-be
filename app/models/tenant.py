0b389f3a9445f4160da652fc7cea4e6dc75fc278
Mubeen Hussain <mubeen.hussain@thetgs.com>
2025-09-16 18:55:55 +0500

    tenant restriction change

--- app/models/tenant.py
+++ app/models/tenant.py
@@ -7,7 +7,7 @@ from app.db.base_class import Base
 
 class Tenant(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    name = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, index=True, nullable=False)
     schema_name = Column(String, unique=True, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     
