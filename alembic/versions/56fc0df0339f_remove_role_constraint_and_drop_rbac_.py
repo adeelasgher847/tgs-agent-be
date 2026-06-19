f3004019b681e9155b02ed296dd750e8a78385bd
Ali Saeed <ali.saeedict@gmail.com>
2026-06-19 23:18:38 +0500

    fix: correct rbacrole table name in migration and merge alembic heads

--- alembic/versions/56fc0df0339f_remove_role_constraint_and_drop_rbac_.py
+++ alembic/versions/56fc0df0339f_remove_role_constraint_and_drop_rbac_.py
@@ -21,7 +21,7 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
     op.execute('ALTER TABLE role DROP CONSTRAINT IF EXISTS chk_role_role')
-    op.execute('DROP TABLE IF EXISTS rbac_roles CASCADE')
+    op.execute('DROP TABLE IF EXISTS rbacrole CASCADE')
 
 
 def downgrade() -> None:
